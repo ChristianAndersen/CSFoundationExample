dae5839bd3952e975452aa01df0806b5ce26031b
ChristianAndersen <595510982@qq.com>
2019-08-31 13:03:41 +0800

    AndersenLi change step on new branch step 2

--- CSFoundationExample/CSFoundationExample/ViewController.m
+++ CSFoundationExample/CSFoundationExample/ViewController.m
@@ -19,6 +19,8 @@
     NSLog(@"AndersenLi create new branch");
     NSLog(@"AndersenLi change step on new branch step 1");
     NSLog(@"ChristianAndersen change step 3");
+    NSLog(@"AndersenLi change step on new branch step 2");
+
 }
 
 
